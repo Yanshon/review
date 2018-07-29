40bb99374e60809243768557eb884d1dd69bb36a
mzgd <mzgd@MZNB170018>
2018-07-29 21:52:56 +0800

    提交： 创建了JUnit

---
+++ test/cn/dw/dao/ProductDaoTest.java
@@ -0,0 +1,34 @@
+package cn.dw.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProductDaoTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+}
