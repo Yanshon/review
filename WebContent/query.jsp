ed953d8fcd0715a97ef061593f6015d695f47709
mzgd <mzgd@MZNB170018>
2018-07-31 10:48:09 +0800

    提交了: 1、查询页面query.jsp的样式修改，显示查询后的数据 2、添加了jstl-1.2.jar和standard-1.1.2.jar 3、转发与重定向的区别 4、前台显示后台数据库-（c:forEach） 5、${requestScope.proList}和${product.name} 6、${num.count}

--- WebContent/query.jsp
+++ WebContent/query.jsp
@@ -1,14 +1,34 @@
 <%@ page language="java" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 
-<title>添加商品页面</title>
+<title>查询商品页面</title>
 </head>
 <body>
 	<form action="/Review_01/ProductServlet" method="get">
-		商品价格:<input type="text" name="keyword"></input><br></br>
+		商品名称:<input type="text" name="keyword"><br></br>
 		<button type="submit">提交商品</button>
 	</form>
+	<table width=600 border="1">
+		<tr>
+			<th>编码</th>
+			<th>名称</th>
+			<th>价格</th>
+			<th>备注</th>
+			<th>操作</th>
+		</tr>
+	
+	<c:forEach items="${requestScope.proList}" var ="product">
+	<tr>
+	<td>${num.count}</td>
+	<td>${product.name}</td>
+	<td>${product.price}</td>
+	<td>${product.remark}</td>	
+	<td>删除|更新</td>
+	</tr>
+	</c:forEach>
+	</table>
 </body>
 </html>
--- src/cn/dw/servlet/ProductServlet.java
+++ src/cn/dw/servlet/ProductServlet.java
@@ -3,6 +3,7 @@ package cn.dw.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,22 +29,31 @@ public class ProductServlet extends HttpServlet {
 //只能用来处理get请求
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		//1:获取查询的关键字keyword
 		String keyword =request.getParameter("keyword");
 		//2:调用业务逻辑（此处不需要添加%%）
 		ArrayList<Product> proList =new ArrayList<Product>();
 		proList=productService.queryByName(keyword);
-		for (Product temp:proList) {
-			System.out.println(temp);
-		//3:跳转查询页面,然后显示数据
-		response.sendRedirect(request.getContextPath()+"/query.jsp");
-		}
+		
+		request.setAttribute("proList",proList);
+//		for (Product temp:proList) {
+//			System.out.println(temp);
+//		//3:跳转查询页面,然后显示数据
+//		response.sendRedirect(request.getContextPath()+"/query.jsp");
+		RequestDispatcher dispatcher=request.getRequestDispatcher("/query.jsp");
+		dispatcher.forward(request, response);
 		
 	}
+		
 
-//只能用阿里处理post请求
+
+//只能用来处理post请求
+//request：
+//response：
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		
 		System.out.println("------doPost----");
 		//1.获取前端的数据
