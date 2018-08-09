d6c64de44f45b0e6f07a2f528cdd62d62d9490bf
mzgd <mzgd@MZNB170018>
2018-08-09 08:56:58 +0800

    添加spring包

---
+++ src/cn/dw/listener/InitDataListener.java
@@ -0,0 +1,20 @@
+package cn.dw.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+//ServletContext：
+public class InitDataListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+
+	}
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
